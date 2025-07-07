8899a6a5096002890a8c68f38f342e6372774033
alexanderras84 <alexanderras@gmail.com>
2025-07-07 09:14:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,5 @@
 #!/bin/bash -e
 
-# Validate DoT config
-if [ "${DNSDIST_ENABLE_DOT}" == "true" ]; then
-  VALID_CERT_TYPE_VALUES=("auto-self" "manual")
-  if [[ -z "$DNSDIST_DOT_CERT_TYPE" ]]; then
-    echo "The environment variable DNSDIST_DOT_CERT_TYPE is not set."
-    exit 1
-  fi
-
-  if [[ " ${VALID_CERT_TYPE_VALUES[*]} " =~ " ${DNSDIST_DOT_CERT_TYPE} " ]]; then
-    if [ "${DNSDIST_DOT_CERT_TYPE}" == "auto-self" ]; then
-      /usr/bin/step certificate create dot.snidust.local /etc/dnsdist/certs/tls.pem /etc/dnsdist/certs/tls.key --profile self-signed --subtle --no-password --insecure
-    fi
-  else
-    echo "[ERROR] Invalid value for DNSDIST_DOT_CERT_TYPE: $DNSDIST_DOT_CERT_TYPE"
-    exit 1
-  fi
-fi
-
-if [ -z "${EXTERNAL_IP}" ];
-then
-  echo "[INFO] External IP not set - trying to get IP by myself"
-  EXTERNAL_IP=$(curl -f icanhazip.com)
-  export EXTERNAL_IP
-fi
-
 if [ -z "${DNSDIST_WEBSERVER_PASSWORD}" ];
 then
   echo "[INFO] Dnsdist webserver password not set - generating one"
@@ -41,12 +16,6 @@ then
   echo "[INFO] Generated WebServer API Key: $DNSDIST_WEBSERVER_API_KEY"
 fi
 
-if [ "$INSTALL_DEFAULT_DOMAINS" = true ];
-then
-  echo "[INFO] Installing default domains..."
-  cp -v /var/lib/snidust/domains.d/*.lst /etc/snidust/domains.d/
-fi
-
 echo "[INFO] Generating ACL..."
 set +e
 source generateACL.sh
@@ -59,21 +28,17 @@ echo "[INFO] Generating DNSDist Config..."
 if [ "$DYNDNS_CRON_ENABLED" = true ];
 then
   echo "[INFO] DynDNS Address in ALLOWED_CLIENTS detected => Enable cron job"
-  echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dynDNSCron.sh" > /etc/snidust/dyndns.cron
-  supercronic /etc/snidust/dyndns.cron &
+  echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dynDNSCron.sh" > /etc/minidns/dyndns.cron
+  supercronic /etc/minidns/dyndns.cron &
 fi
 
 echo "[INFO] Starting DNSDist..."
-/usr/bin/dnsdist -C /etc/dnsdist/dnsdist.conf --supervised --disable-syslog --uid snidust --gid snidust &
+/usr/bin/dnsdist -C /etc/dnsdist/dnsdist.conf --supervised --disable-syslog --uid minidns --gid minidns &
 
 
-echo "[INFO] Starting nginx.."
-nginx
-nginx_processId=$!
 
 sleep 5
 
 echo "==================================================================="
-echo "[INFO] SniDust started => Using $EXTERNAL_IP - Point your DNS settings to this address"
+echo "[INFO] MiniDNS started => Point your DNS settings to this address"
 echo "==================================================================="
-wait $nginx_processId
