2e9b301341592e840c960c2924e33b50327ba6ff
alexanderras84 <alexanderras@gmail.com>
2025-07-12 22:03:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-if [ -z "${DNSDIST_WEBSERVER_PASSWORD}" ];
-then
+# Generate webserver password if not set
+if [ -z "${DNSDIST_WEBSERVER_PASSWORD}" ]; then
   echo "[INFO] Dnsdist webserver password not set - generating one"
   DNSDIST_WEBSERVER_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12)
   export DNSDIST_WEBSERVER_PASSWORD
   echo "[INFO] Generated WebServer Password: $DNSDIST_WEBSERVER_PASSWORD"
 fi
 
-if [ -z "${DNSDIST_WEBSERVER_API_KEY}" ];
-then
+# Generate webserver API key if not set
+if [ -z "${DNSDIST_WEBSERVER_API_KEY}" ]; then
   echo "[INFO] Dnsdist webserver api key not set - generating one"
   DNSDIST_WEBSERVER_API_KEY=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
   export DNSDIST_WEBSERVER_API_KEY
@@ -17,18 +17,22 @@ then
 fi
 
 echo "[INFO] Generating ACL..."
-set +e
-source generateACL.sh
-set -e
 
+# Prepare ACL files and ensure correct permissions before generating ACL
+touch /etc/dnsdist/allowedClients.acl /etc/dnsdist/allowedClients.conf
+chown minidns:minidns /etc/dnsdist/allowedClients.acl /etc/dnsdist/allowedClients.conf
+chmod 644 /etc/dnsdist/allowedClients.acl /etc/dnsdist/allowedClients.conf
+
+set +e
+source /generateACL.sh
+set -e
 
 echo "[INFO] Generating DNSDist Config..."
 /bin/bash /etc/dnsdist/dnsdist.conf.template > /etc/dnsdist/dnsdist.conf
 
-if [ "$DYNDNS_CRON_ENABLED" = true ];
-then
+if [ "${DYNDNS_CRON_ENABLED:-false}" = true ]; then
   echo "[INFO] DynDNS Address in ALLOWED_CLIENTS detected => Enable cron job"
-  echo "$DYNDNS_CRON_SCHEDULE /bin/bash /dynDNSCron.sh" > /etc/minidns/dyndns.cron
+  echo "${DYNDNS_CRON_SCHEDULE:-*/1 * * * *} /bin/bash /dynDNSCron.sh" > /etc/minidns/dyndns.cron
   supercronic /etc/minidns/dyndns.cron &
 fi
 
